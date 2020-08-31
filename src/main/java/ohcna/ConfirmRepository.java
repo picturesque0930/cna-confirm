5766ddb35b99262d2214b5bb675394b691a4c224
picturesque0930 <32059878+picturesque0930@users.noreply.github.com>
2020-08-31 17:32:04 +0900

    Add files via upload
    include resource commit

---
+++ src/main/java/ohcna/ConfirmRepository.java
@@ -0,0 +1,8 @@
+package ohcna;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ConfirmRepository extends PagingAndSortingRepository<Confirm, Long>{
+
+
+}
---
+++ src/main/java/ohcna/PolicyHandler.java
@@ -0,0 +1,26 @@
+package ohcna;
+
+import ohcna.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverBookingCreated_ConfirmRequest(@Payload BookingCreated bookingCreated){
+
+        if(bookingCreated.isMe()){
+            System.out.println("##### listener ConfirmRequest : " + bookingCreated.toJson());
+        }
+    }
+
+}
